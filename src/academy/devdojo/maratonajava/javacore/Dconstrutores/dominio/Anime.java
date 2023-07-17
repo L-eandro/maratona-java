c1c7b1ad35b5f11f903d4403423a2e85f37501fa
Leandro José <leandrojose.a98@gmail.com>
2023-07-16 23:53:39 -0300

    Construtores pt01

---
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.dominio;
+
+public class Anime {
+    private String nome;
+    private String tipo;
+    private int episodios;
+    private String genero;
+
+    public Anime(String nome, String tipo, int episodios, String genero) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+        this.genero = genero;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+    }
+
+}
