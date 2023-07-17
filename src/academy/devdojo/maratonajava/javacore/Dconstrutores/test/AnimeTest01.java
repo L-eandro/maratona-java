572e9ac8034a953ac985c0d217e0719daaedf519
Leandro José <leandrojose.a98@gmail.com>
2023-07-17 00:48:09 -0300

    :white_check_mark:  Construtores pt.02 - Sobrecarga

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -5,7 +5,7 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação");
+        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação", "Mad House");
         anime.imprime();
 
     }
